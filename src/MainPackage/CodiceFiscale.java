151e8d73ae5728a1092ac975acd365c0ca2614ea
TuxDave <godinodavide@gmail.com>
2020-02-23 13:43:05 +0100

    Completata la creazione di tutta la classe per calcolare il codice fiscale. Bug noti: non presenti

--- src/MainPackage/CodiceFiscale.java
+++ src/MainPackage/CodiceFiscale.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.TreeMap;
 
 public class CodiceFiscale{
     private String cognome;
@@ -159,5 +160,41 @@ public class CodiceFiscale{
             d = Integer.toString(ret);
         }
         return d;
+    }
+    private String codeParziale(){
+        return new String(this.codeCognome() + this.codeNome() + this.codeYear() + this.codeMese() + this.codeDay() + this.cc);
+    }
+    private String codeCDC(){
+        TreeMap<Character, Integer> dispari = new TreeMap<>();
+        TreeMap<Character, Integer> pari = new TreeMap<>();
+        String stringaDispari = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String stringaPari = stringaDispari;
+        int[] interiDispari = {1,0,5,7,9,13,15,17,19,21,1,0,5,7,9,13,15,17,19,21,2,4,18,20,11,3,6,8,12,14,16,10,22,25,24,23};
+        int[] interiPari = {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+        for(int i = 0; i < stringaDispari.length(); i++){
+            dispari.put(stringaDispari.toCharArray()[i], interiDispari[i]);
+            pari.put(stringaPari.toCharArray()[i], interiPari[i]);
+        }
+        String p1 = "", p2 = "", parziale = this.codeParziale();
+        int s = 0;
+        for (int i = 0; i < 15; i++){
+            if (i % 2 == 0){ //dispari
+                p1 += parziale.toCharArray()[i];
+            }else{ //pari
+                p2 += parziale.toCharArray()[i];
+            }
+        }
+        for(char lettera : p1.toCharArray()){ //dispari
+            s += dispari.get(lettera);
+        }
+        for(char lettera : p2.toCharArray()){ //pari
+            s += pari.get(lettera);
+        }
+        p1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        s = s % 26;
+        return Character.toString(p1.toCharArray()[s]);
+    }
+    public String codeCF(){
+        return new String(this.codeParziale() + this.codeCDC());
     }
 }
--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] argv) {
         CodiceFiscale cf = null;
         try{
-            cf = new CodiceFiscale("", "", "01/01/1999", 'f', "villasor", "su");
+            cf = new CodiceFiscale("", "", "01/01/1999", 'm', "torino", "to");
         } catch (IOException e) {
             String m = e.getMessage();
             System.out.println(m);
         }catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
-        System.out.println(cf.codeCognome() + cf.codeNome() + cf.codeYear() + cf.codeMese() + cf.codeDay());
+        System.out.println(cf.codeCF());
     }
 }
